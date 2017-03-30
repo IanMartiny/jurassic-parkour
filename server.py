c89a2f622e604e07eeb4f3cf42af874b8aa1c668
Ian Martiny <ian.martiny@colorado.edu>
2017-03-30 15:52:56 -0600

    add single entry point for client to update threshold and increase threshold function

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import random
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,9 +14,27 @@ def js():
 def css():
     return app.send_static_file("./index.css")
 
-@app.route('/randomThresh/')
+@app.route('/updateThresh/', methods=["POST"])
+def updateThresh():
+    model = request.form["model"]
+    if model == "random":
+        return randThresh()
+    elif model == "increase":
+        return incrementThresh(float(request.form["distance"]), float(request.form["thresh"]))
+
+# @app.route('/randomThresh/')
 def randThresh():
     return jsonify({'thresh': random.random()})
+
+# @app.route('/incrementThresh/', methods=["POST"])
+def incrementThresh(score, thresh):
+    if 1000 <= score <= 2000:
+        return jsonify({'thresh': thresh + (1-thresh)/10})
+    elif 2001 <= score <= 4000:
+        return jsonify({'thresh': thresh + (1-thresh)/50})
+    else:
+        return jsonify({'thresh': thresh + (1-thresh)/100})
+
 
 @app.route('/assets/<path:path>')
 def sendAssets(path):
